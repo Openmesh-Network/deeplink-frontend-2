9650d900e562b742c61d19f1bdedb1fffb724fa0
Bruno Santos <brunolsantos152@gmail.com>
2023-06-27 22:10:44 -0300

    feat: deeplink setup

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -53,20 +53,21 @@ const Header = () => {
                   sticky ? "py-5 lg:py-2" : "py-8"
                 } `}
               >
-                <Image
+                {/* <Image
                   src="/images/logo/logo-2.svg"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
-                />
-                <Image
+                /> */}
+                <h1> DeepLink</h1>
+                {/* <Image
                   src="/images/logo/logo.svg"
                   alt="logo"
                   width={140}
                   height={30}
                   className="hidden w-full dark:block"
-                />
+                /> */}
               </Link>
             </div>
             <div className="flex w-full items-center justify-between px-4">
