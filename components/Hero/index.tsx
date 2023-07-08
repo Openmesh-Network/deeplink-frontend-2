5d4555792e612ac3db0c0fd5abb9b5f6c29e0729
Bruno Santos <brunolsantos152@gmail.com>
2023-07-08 12:09:18 -0300

    feat: new changes front

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
-import Image from 'next/image'
+// import Image from 'next/image'
 
 const Hero = () => {
   return (
@@ -12,11 +12,11 @@ const Hero = () => {
                 <h3 className="mb-4 text-xl font-bold text-black sm:text-2xl lg:text-2xl xl:text-4xl">
                   Research & Development
                 </h3>
-                <p className="text-base font-light leading-relaxed text-black sm:text-base sm:leading-relaxed">
+                <p className="text-xs font-light leading-relaxed text-black sm:text-sm sm:leading-relaxed">
                   All-in-one platform for coordinating, displaying, and managing
                   tasks, jobs, and projects.
                 </p>
-                <p className="mt-4 text-base font-light leading-relaxed text-black sm:text-base sm:leading-relaxed">
+                <p className="mt-4 text-xs font-light leading-relaxed text-black sm:text-sm sm:leading-relaxed">
                   We welcome all community developers to support our project.
                   You can view the desired department for project outlines,
                   budgets, deadlines, and project details. All projects are
@@ -26,7 +26,7 @@ const Hero = () => {
                   platforms like Upwork or Freelancer, where Upwork provides
                   support. In our case, the smart contract takes care of that.
                 </p>
-                <p className="mt-4 text-base font-medium leading-relaxed text-black sm:text-lg sm:leading-relaxed">
+                <p className="mt-4 text-xs font-light leading-relaxed text-black sm:text-sm sm:leading-relaxed">
                   Tags: Smart contract, Ethereum adapters, front end, backend
                   design, documentation, running event
                 </p>
@@ -35,23 +35,26 @@ const Hero = () => {
           </div>
           <div className="w-full px-4 lg:w-1/3">
             <div
-              className="wow fadeInUp border-gray-300 relative mx-auto max-w-[500px] rounded-md bg-[#FAF7F7] p-4 text-center text-black shadow-xl lg:m-0"
+              className="wow fadeInUp border-gray-300 relative mx-auto max-w-[500px] rounded-md bg-[#FAF7F7] p-4 text-center text-base text-black shadow-xl lg:m-0"
               data-wow-delay=".15s"
             >
-              <div className="flex">
+              <div className="mb-12 flex">
                 <p>Available Funding</p> <p className="ml-4">1200</p>
               </div>
               <div>
-                <div className="flex">
-                  <p>No. of open projects:</p> <p className="ml-4">14</p>
+                <div className="flex border-b-[1px] border-[#bcbaba]">
+                  <p>No. of open projects:</p>{' '}
+                  <p className="ml-4 mt-[2px] text-sm">14</p>
+                </div>
+
+                <div className="flex border-b-[1px] border-[#bcbaba]">
+                  <p>No. of completed projects:</p>{' '}
+                  <p className="ml-4 mt-[2px] text-sm">14</p>
                 </div>
 
                 <div className="flex">
-                  <p>No. of completed projects:</p> <p className="ml-4">14</p>
-                </div>
-
-                <div className="flex">
-                  <p>Current members:</p> <p className="ml-4">14</p>
+                  <p>Current members:</p>{' '}
+                  <p className="ml-4 mt-[2px] text-sm">14</p>
                 </div>
               </div>
             </div>
