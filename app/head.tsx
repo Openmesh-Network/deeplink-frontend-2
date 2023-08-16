ec06490a759409a5ba4091612deff94d7d2d21ab
Bruno Santos <brunolsantos152@gmail.com>
2023-08-16 15:59:19 -0300

    fix: fixing header on link sharing

--- app/head.tsx
+++ app/head.tsx
@@ -6,12 +6,8 @@ export default function Head() {
       {/* <meta content="width=device-width, initial-scale=1" name="viewport" /> */}
       <meta property="og:image" content="/images/testimonials/author-03.png" />
       <meta
-        name="description"
-        content="Open-source platform designed to empower decentralized teams to collaborate seamlessly.23"
-      />
-      <meta
         property="og:description"
-        content="Open-source platform designed to empower decentralized teams to collaborate seamlessly.13"
+        content="Open-source platform designed to empower decentralized teams to collaborate seamlessly."
       />
       <link rel="icon" href="/images/redLogo.ico" />
     </>
