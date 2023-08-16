575e7ee2a1ce9319d58fef0fd305e3fab927a6fc
Bruno Santos <brunolsantos152@gmail.com>
2023-08-16 15:12:36 -0300

    fix: fixing header on link sharing

--- app/head.tsx
+++ app/head.tsx
@@ -3,8 +3,8 @@ export default function Head() {
   return (
     <>
       <title>OpenR&D</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta property="og:image" content="/images/open.png" />
+      {/* <meta content="width=device-width, initial-scale=1" name="viewport" /> */}
+      <meta property="og:image" content="/images/testimonials/author-03.png" />
       <meta
         name="description"
         content="Open-source platform designed to empower decentralized teams to collaborate seamlessly."
