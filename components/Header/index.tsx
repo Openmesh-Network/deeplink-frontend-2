93b5b5373d66b16053769cf9ebab5ec3578047de
Bruno Santos <brunolsantos152@gmail.com>
2023-09-20 15:50:57 -0300

    feat: change header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -37,7 +37,7 @@ const Header = () => {
   // Este useEffect está travando a aplicação, econtrar maneira de tratar isto.
   useEffect(() => {
     window.addEventListener('scroll', handleStickyNavbar)
-    if (chain && chain.name !== 'Polygon Mumbai') {
+    if (chain && chain.name !== process.env.NEXT_PUBLIC_WALLET_ENVIRONMENT) {
       setIsChainWrong(true)
       setTheme({
         themeMode: 'dark',
