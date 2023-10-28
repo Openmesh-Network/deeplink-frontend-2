02256aa9380bf31cba67fc999218deef7a40f7ba
Bruno Santos <brunolsantos152@gmail.com>
2023-10-27 23:29:35 -0300

    feat: new type of logo try

--- components/NewFooter/index.tsx
+++ components/NewFooter/index.tsx
@@ -23,7 +23,7 @@ const NewFooter = () => {
                   : ''
               }/images/header/logo.svg`}
               alt="image"
-              className={`w-[91px] md:w-[110px] lg:w-[128px] xl:w-[146px] 2xl:w-[183px]`}
+              className={`w-[240px] md:w-[110px] lg:w-[128px] xl:w-[146px] 2xl:w-[183px]`}
             />
             <div className="mt-[15px] text-[12px] font-normal md:mt-[18px] md:text-[14.5px] lg:mt-[21px] lg:text-[17px] lg:!leading-[29px] xl:mt-[24px] xl:text-[19px] 2xl:mt-[30px] 2xl:text-[24px]">
               Building open-source decentralized data infrastructure in Web2 and
